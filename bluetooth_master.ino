8d74329d66e37aa1faeabd127b0f2a33d4dcd355
uclatonystark <35809048+uclatonystark@users.noreply.github.com>
2018-04-23 06:42:13 -0700

    Add files via upload
    bluetooth_master.ino: upload this file to master (line following robot)
    bluetooth_slave.ino: upload this file to slave (drawing robot)
    NOTE: when uploading these files to the Arduinos, make sure to UNPLUG TX AND RX on the Arduinos.

---
+++ bluetooth_master.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write(9);
+  delay(200);
+  Serial.write(0);
+  delay(200);
+}
---
+++ bluetooth_slave.ino
@@ -0,0 +1,28 @@
+#define ledPin 13
+int state=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,HIGH);
+  Serial.begin(38400);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    state=Serial.read();
+  }
+
+
+  if(state==9){
+    digitalWrite(ledPin,HIGH);
+    Serial.println("9");
+    state=0;
+  }
+  else if(state==0){
+    digitalWrite(ledPin,LOW);
+    Serial.println("0");
+    state=0;
+  }
+  delay(200);
+}
